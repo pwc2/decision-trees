325b3832c3eeb84b978aaa14b852d180bf17fbf5
Patrick Cummings <42842025+pwc2@users.noreply.github.com>
2019-11-20 22:06:35 -0800

    moved run scripts to run/

--- main.py
+++ main.py
@@ -7,12 +7,9 @@
     Python Version: 3.7
 
 """
-
-import run_part1
+from run import run_part1
 print('Part 1: decision trees, complete.')
-
-import run_part2
+from run import run_part2
 print('Part 2: random forests, complete.')
-
-import run_part3
+from run import run_part3
 print('Part 3: boosted trees, complete.')
